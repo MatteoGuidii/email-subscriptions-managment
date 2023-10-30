4ff547c705052b6b1bcb607f048c1fefcedf345d
Matteo Guidi <matteoguidi55@gmail.com>
2023-10-29 22:40:39 -0400

    Adjusted Auth.js and created Auth.css

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import './Auth.css';
+
 const Auth = () => {
   const [isLogin, setIsLogin] = useState(true);
 
@@ -9,16 +11,24 @@ const Auth = () => {
 
   return (
     <div className="container">
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
-        <input type="email" placeholder="Email" required />
-        <input type="password" placeholder="Password" required />
-        { !isLogin && <input type="password" placeholder="Confirm Password" required /> }
-        <button type="submit">{isLogin ? 'Login' : 'Sign Up'}</button>
-      </form>
-      <button onClick={switchAuthModeHandler}>
-        Switch to {isLogin ? 'Sign Up' : 'Login'}
-      </button>
+      <div className="auth-container">
+        <div className="info-panel">
+          <h1>Welcome to MyApp</h1>
+          <p>Manage your email subscriptions with ease.</p>
+        </div>
+        <div className="auth-card">
+          <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+          <form>
+            <input type="email" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            {!isLogin && <input type="password" placeholder="Confirm Password" required />}
+            <button type="submit">{isLogin ? 'Login' : 'Sign Up'}</button>
+          </form>
+          <button className="toggle-btn" onClick={switchAuthModeHandler}>
+            Switch to {isLogin ? 'Sign Up' : 'Login'}
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
